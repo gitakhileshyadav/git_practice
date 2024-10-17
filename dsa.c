81c8e3b2aa9198fee6afc461b6d39c8e3a4fea8c
Akhilesh <akhileshyadav805a@gmail.com>
2024-10-17 08:23:25 +0530

    update index.html for resume filter

---
+++ dsa.c
@@ -0,0 +1,17 @@
+/*
+//constant space complexity
+#include<stdio.h>
+int square(int n){
+    printf("%d Bytes\n",sizeof(int));
+    printf("%d Bytes\n",sizeof(n));
+    return n*n;
+}
+main(){
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    int value=square(num);
+    printf("%d",value);
+}
+*/
+
